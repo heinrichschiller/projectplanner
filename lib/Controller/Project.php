858e970304f728b529c417573f8a8b0c6af706b5
heinrichschiller <heinrich@heinrich-schiller.de>
2018-10-23 17:03:58 +0200

    update, projects functionality

--- lib/Controller/Project.php
+++ lib/Controller/Project.php
@@ -2,8 +2,9 @@
 
 namespace Controller;
 
-use \Model\Resource\Project as ProjectResource;
+use \Model\Resource\Project  as ProjectResource;
 use \Model\Resource\Customer as CustomerResource;
+use \Model\Resource\Task     as TaskResource;
 
 class Project extends Base
 {
@@ -25,17 +26,33 @@ class Project extends Base
         echo $this->render('project.phtml', array('customers' => $customerList));
     }
 
-    public function addAction($params)
+    public function addAction()
     {
         $project = new ProjectResource();
 
         $project->add($_POST);
 
-        header('Location: http://projektmanager.localhost/project/index');
+        header('Location: ' . \App::getBaseUrl() . 'project/index');
     }
 
     public function viewAction($params)
     {
+        $customer = new CustomerResource();
+        $project = new ProjectResource();
+        $task = new TaskResource();
+        
+        $projectList = $project->getProjectById($params['id']);
 
+        $id = $projectList->getCustomerId();
+        $customerList = $customer->getCustomer($id);
+        $taskList = $task->getCustomerTasks($id);
+
+        $list = [
+            'customer' => $customerList,
+            'project'  => $projectList,
+            'tasks'    => $taskList
+        ];
+        
+        echo $this->render('project_overview.phtml', $list);
     }
 }
