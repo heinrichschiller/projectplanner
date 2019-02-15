847e113a91f4db08bfd426d8c556e538c7741337
Heinrich Schiller <heinrich@heinrich-schiller.de>
2019-02-15 14:54:28 +0100

    #CHANGE 'view update'

--- skin/js/tables.js
+++ skin/js/tables.js
@@ -13,14 +13,14 @@ $(function() {
     $("#projects-table td:contains('In Bearbeitung')").addClass("project-inprocessing");
     $("#projects-table td:contains('Warten')").addClass("task-active");
     $("#projects-table td:contains('Abgeschlossen')").addClass("task-active");
-    $("#projects-table td:contains('Eingestellt')").addClass("task-active");
+    $("#projects-table td:contains('Eingestellt')").addClass("task-suspend");
 
     $("#projects-table td:contains('Niedrig')").addClass("not-important");
     $("#projects-table td:contains('In Plannung')").addClass("in-planning");
     $("#projects-table td:contains('Sehr Hoch')").addClass("very-important");
 
-    $("table tr").click(function() {
-        var href = $(this).find("a").attr("href");
+    $(".clickable-row").click(function() {
+        var href = $(this).data("href");
 
         if(href) {
             window.location = href;
